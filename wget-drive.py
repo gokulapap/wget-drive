7fb8c5c6d4c2b5c885356864c557ac2fba85f8ec
shyam0904a <shyamganesh01@gmail.com>
2021-10-29 01:48:28 +0530

    Windows Script Partial patch changing wget to curl and identifying the platform running

--- wget-drive.py
+++ wget-drive.py
@@ -1,8 +1,9 @@
 #/usr/bin/python3
 #script to download drive file links by wget
 
-from os import system
 
+import os
+import platform
 link = input("Enter the drive link : ")
 out = input("Enter the output file name: ")
 size = input("whether the file size is less than 100MB ? (Y/N) : ")
@@ -10,9 +11,11 @@ size = input("whether the file size is less than 100MB ? (Y/N) : ")
 parts = link.split('/')
 fileid = parts[5]
 
-# if the size is less than 100MB - Y from input
-if size == 'Y' or size == 'y':
-  system("wget --no-check-certificate 'https://docs.google.com/uc?export=download&id={}' -O {}".format(fileid, out))
-
-else:
-  system("wget --load-cookies \/tmp\/cookies.txt \"https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id={}' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id={}\" -O {} && rm -rf /tmp/cookies.txt".format(fileid, fileid, out))
+if platform.system()=='Linux' or platform.system()=='Darwin':
+  if size == 'Y' or size == 'y':
+    os.system("wget --no-check-certificate 'https://docs.google.com/uc?export=download&id={}' -O {}".format(fileid, out))
+  else:
+    os.system("wget --load-cookies \/tmp\/cookies.txt \"https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id={}' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id={}\" -O {} && rm -rf /tmp/cookies.txt".format(fileid, fileid, out))
+elif platform.system()=='Windows':
+  if size == 'Y' or size == 'y':
+    os.system("curl -K 'https://docs.google.com/uc?export=download&id={}' -O {}".format(fileid, out))
